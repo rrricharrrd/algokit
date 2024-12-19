796c5616c2a5ff24f128b6b6c4aa0e7cc466542c
Richard Harris <rrricharrrd@gmail.com>
2024-12-19 17:08:05 +0100

    MAINT: bloom filter remove numpy dep

--- algokit/data_structures/probabilistic/_bloom_filter.py
+++ algokit/data_structures/probabilistic/_bloom_filter.py
@@ -2,8 +2,6 @@ import hashlib
 import random
 from typing import Any, Callable, Optional
 
-import numpy as np
-
 
 def _make_hash_function(salt: Any) -> Callable:
     def hash_function(x: Any) -> int:
@@ -19,16 +17,16 @@ class BloomFilter:
         self._rng = rng or random.Random(123)
         self._hash_functions = [_make_hash_function(i) for i in self._rng.sample(range(0, 10 * n_filters), n_filters)]
         self._filter_size = filter_size
-        self._buckets = np.zeros((n_filters, filter_size))
+        self._buckets = [[0 for _ in range(filter_size)] for _ in range(n_filters)]
 
     def add(self, x: Any) -> None:
         for ix, hf in enumerate(self._hash_functions):
             h = hf(x) % self._filter_size
-            self._buckets[ix, h] = 1
+            self._buckets[ix][h] = 1
 
     def query(self, x: Any) -> bool:
         for ix, hf in enumerate(self._hash_functions):
             h = hf(x) % self._filter_size
-            if self._buckets[ix, h] != 1:
+            if self._buckets[ix][h] != 1:
                 return False
         return True
